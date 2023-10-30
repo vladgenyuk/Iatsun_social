c412bf6f489035bae64b29d8631a8ba19e9cb65c
vlad <vlad.genyuk@mail.ru>
2023-10-30 19:12:51 +0300

    Final models + images

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-class Solution:
-    def decodeAtIndex(self, s: str, k: int) -> str:
-        tape = ''
+h1 = {"1": "2", "3": "4"}
+h2 = {"3": "6"}
 
-        for i in s:
-            if len(tape) > k:
-                return tape[k-1]
-            if i not in '1234567890':
-                tape += i
-            else:
-                tape = tape * int(i)
-
-        return tape[k-1]
-
-
-s = Solution()
-
-print(s.decodeAtIndex('leet2code3', 10))
-print(s.decodeAtIndex('ha22', 5))
-print(s.decodeAtIndex('a234567899999999', 10))
+print(dict(h1, **h2))
